44f05e435b0066fb97141f7b1e3cc1a9cd3b167a
mohammed waleed <mwj192@gmail.com>
2024-02-25 23:43:50 +0300

    a2

--- my_project/APP44/views.py
+++ my_project/APP44/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def default(request)
     return render(request, 'APP44/default.html')
+
+def calculate_tax(request, any_number):
+    tax_rate = 0.15
+    price = float(any_number)
+    total = price + (price * tax_rate)
+    return HttpResponse(f'Total price with 15% tax: {total}')
+def tax_rate_view(request):
+    tax_rate = 0.15
+    return render(request, 'APP44/taxRate.html', {'tax_rate': tax_rate})
+
